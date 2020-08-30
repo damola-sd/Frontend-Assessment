73f0b088055d88e7ce2f0d75f5b3623cec5961d2
Damola Adewunmi <damolasd@gmail.com>
2020-08-30 03:04:30 +0100

    changed db to postgres

--- data-layer/data_layer/settings.py
+++ data-layer/data_layer/settings.py
@@ -1,6 +1,9 @@
+from decouple import config
 import os
 
+
+os.environ['DATABASE_URL'] = 'postgres://lgllyayu:pBmRfXVFZ60eIRBrkvwjLefMVGWo_rPZ@rogue.db.elephantsql.com:5432/lgllyayu'
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.join(BASE_DIR, "..", "..", "example.db")
-DATABASE_URL = os.getenv("DATABASE_URL", f"sqlite:///{ROOT_DIR}")
+DATABASE_URL = config('DATABASE_URL')
 print(DATABASE_URL)
