6424fb9c8b53b310188306218d7162c45bd7b08d
Damola Adewunmi <damolasd@gmail.com>
2020-08-31 14:40:40 +0100

    feat: added routes

--- services/example/src/notes.ts
+++ services/example/src/notes.ts
@@ -12,4 +12,46 @@ router.post("/create", async (req, res) => {
 	res.status(200).json(result);
 });
 
+router.get("/", async (req, res) => {
+	try {
+		const prisma: PrismaClient = req.app.locals.prisma;
+		const results = await prisma.notes.findMany();
+
+		res.status(200).json(results)
+
+	} catch (err) {
+		res.status(500).json(err.message);
+	}
+
+});
+
+router.put("/:noteId", async (req, res) => {
+	try {
+		const prisma: PrismaClient = req.app.locals.prisma;
+		const { description, title } = req.body;
+		const { noteId } = req.params;
+		const result = await prisma.notes.update({
+			where: { id: Number(noteId) },
+			data: { description, title }
+		});
+		res.status(200).json(result);
+	} catch (err) {
+		res.status(500).json(err.message);
+	}
+
+});
+
+router.delete("/:noteId", async (req, res) => {
+	try {
+		const prisma: PrismaClient = req.app.locals.prisma;
+		const deleted = await prisma.notes.delete({
+			where: { id: Number(req.params.noteId) }
+		});
+		res.status(200).json(deleted);
+	} catch (err) {
+		res.status(500).json(err.message);
+	}
+
+});
+
 export default router;
